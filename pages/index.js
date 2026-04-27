e50d7dec9e8e76cf99f7e94bb5ca81655cac7fb8
FabioCKM <fabio.machmora@hotmail.com>
2026-04-27 19:11:05 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 function Home(){
-    return <h1>Boa tarde </h1>
+    return <h1 id="btn">Larissa clique em mim</h1>
 }
 export default Home;
+
+let botao = document.getElementById("btn");
+
+botao.addEventListener("click",()=>{
+    botao.innerText = "Boa Tarde";
+});
